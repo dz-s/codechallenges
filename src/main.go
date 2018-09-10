611970f591ed2c6083700b544aa896dba9bf671f
Dzmitry Shulhin <shulhin_dv@local.st.by>
2018-09-10 16:54:40 +0300

    binary gap

---
+++ src/main.go
@@ -0,0 +1,37 @@
+package main
+
+import "fmt"
+
+//Solution for BinaryGap task
+func Solution(N int) int {
+	var maxGap int = 0
+	var currGap int = 0
+	var insideGap = false
+
+	if N > 0 {
+
+		for i := N; i > 0; i /= 2 {
+			if i%2 == 0 {
+				currGap++
+			} else {
+				if insideGap {
+					if currGap > maxGap {
+						maxGap = currGap
+					}
+					currGap = 0
+				} else {
+					insideGap = true
+				}
+
+			}
+		}
+	}
+
+	return maxGap
+
+}
+
+func main() {
+	fmt.Printf("result for of 1041: %d\n", Solution(1041))
+	fmt.Printf("result for of 32: %d\n", Solution(32))
+}
